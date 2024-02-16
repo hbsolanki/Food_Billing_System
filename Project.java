b0819862b3bc1d94644fe1df637c2611bdf5dac2
Hardik Solanki <136411045+hbsolanki@users.noreply.github.com>
2024-02-16 15:47:18 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,175 +1,145 @@
 import java.util.*;
-class Project
-{	
-	static int bill;
-	static int cnt=1;
-	static String name;
+class FoodBill {
+    static String item[]=new String[]{"Onions Pizza","Tomatoes Pizza","Capsicum Pizza","Sweet Corn Pizza","Onions&Panner Pizza","Margheta Pizza","Ala Mexican Pizza","Authentic Pizza","Burn To Hell Pizza","Korma Panner Special Pizza","Cheesy Macaroni Veg"};
+    static int price[] = new int[] { 89, 89, 99, 105, 129, 140, 255, 255, 255, 255, 285 };
+    static int orderPizzaNumber []= new int[10];
+    static int orderPizzaquantity[]= new int[10];
+    static Scanner sc = new Scanner(System.in);
+    static int orderNumber = 0;
+    static String name;
 	static String mobile_number;
-    static int order[]=new int[0];
-	static int order_qun[]=new int[0];
-	static int tempbill[]=new int[0];
-	static String item[]=new String[]{"Onions Pizza","Tomatoes Pizza","Capsicum Pizza","Sweet Corn Pizza","Onions&Panner Pizza","Margheta Pizza","Ala Mexican Pizza","Authentic Pizza","Burn To Hell Pizza","Korma Panner Special Pizza","Cheesy Macaroni Veg"};
-	static int price[]=new int[]{89,89,99,105,129,140,255,255,255,255,285};
-	
-		//needed variable and array defined
-	
-	void welcome()
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println();
+    public static void main(String[] args) {
+        welcome();
+        int choice;
+        do {
+            System.out.println();
+            System.out.println("(1)Order Pizza \n(2)Show Cart \n(3)Bill Generate \n(4)Exit");
+            System.out.println();
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    orderPizza();
+                    break;
+                case 2:
+                    showCart();
+                    break;
+
+                case 3:
+                    billGenerate();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Enter Valid Option");
+            }
+        } while (choice != 4);
+    }
+    
+    public static void welcome() {
+        System.out.println();
 		System.out.println("***** Welcome In Our Shop *****");
 		System.out.print("Enter Your Name:");
 		name=sc.nextLine();
+
 		boolean flag=false;
-	   //checking Mobile Number Valid
-		do
-		{
-			System.out.print("Enter Mobile Number:");
-			mobile_number=sc.nextLine();
-			
-			if(mobile_number.length()==10)
-			{
-				for(int i=0;i<mobile_number.length();i++)
-				{
-					if(mobile_number.charAt(i)>='0' && mobile_number.charAt(i)<='9')
-					{
+        do{
+            System.out.print("Enter Mobile Number:");
+                mobile_number=sc.nextLine();
+                
+			if(mobile_number.length()==10){
+				for(int i=0;i<mobile_number.length();i++){
+					if(mobile_number.charAt(i)>='0' && mobile_number.charAt(i)<='9'){
 						flag=true;	
-					} else
-					{
+					} else{
 						flag=false;
 						System.out.println("Enter A Valid Number");
 					}
 				}
-			} else
-			{
+			} else{
 				System.out.println("Enter A Valid NUmber");
 				flag=false;
 			}
 		}while(flag==false);
 		
-		food();
-	}
-	
-	void food()
-	{
-		System.out.println();	
-		System.out.println("In Our Shop Given Pizza Are Avilable");
-		
-		System.out.println();
-		for(int i=0;i<item.length;i++)
-		{	
-		System.out.println((i+1)+")"+item[i]+" - "+price[i]+"inr");
-		}
-		System.out.println();
-		
-		order_take();
-	}
- 	
-	void order_take()
-	{		
-		System.out.println("If You Want To Add Food Then Type Food's Number Else Exit For 0");
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		
-		if(n<0||n>11)
-		{
-			System.out.println("invalid Option, Choice Valid Option");
-			order_take();
-		}
-		
-		if(n==0)
-		{
-			bill_Cal();
-			return;
-		}
-		System.out.println("Enter Quantity");
-		int q=sc.nextInt();
-		int price[]=new int[]{89,89,99,105,129,140,255,255,255,255,285};
-		
-		bill+=price[n-1]*q;
-		
-		//increece array Size Use Temp 
-		
-		if(order.length<cnt)
-		{
-			int temp[]=new int[order.length];
-			for(int i=0;i<order.length;i++)
-			{
-				temp[i]=order[i];
-			}
-			order=new int[cnt+1];
-			for(int i=0;i<temp.length;i++)
-			{
-				order[i]=temp[i];
-			}
-		}
-		if(order_qun.length<cnt)
-		{
-			int temp2[]=new int[order.length];
-			for(int i=0;i<order_qun.length;i++)
-			{
-				temp2[i]=order_qun[i];
-			}
-			order_qun=new int[cnt+1];
-			for(int i=0;i<temp2.length;i++)
-			{
-				order_qun[i]=temp2[i];
-			}
-		}
-		if(tempbill.length<cnt)
-		{
-			int temp3[]=new int[order.length];
-			for(int i=0;i<tempbill.length;i++)
-			{
-				temp3[i]=tempbill[i];
-			}
-			tempbill=new int[cnt+1];
-			for(int i=0;i<temp3.length;i++)
-			{
-				tempbill[i]=temp3[i];
-			}
-		}	
-		
-		order[cnt-1]=n;
-		order_qun[cnt-1]=q;
-		tempbill[cnt-1]=(price[n-1]*q);
-		 cnt++;
-		
-		order_take(); 
-		 
-	}
-	
-	void bill_Cal()
-	{
-		System.out.println("----------------------------------------------------------------");
+    }
+    
+    public static void orderPizza() {
+
+        if (orderNumber >= 10) {
+            billGenerate();
+        }
+
+
+        System.out.println("Available pizza");
+        System.out.println();
+        for (int i = 0; i < item.length; i++) {
+            System.out.println((i + 1) + " " + item[i] + " " + price[i]+"₹");
+        }
+        System.out.println("If You Want To Add Food Then Type Food's Number Else Exit For 0");
+        int choicePizzaNumber = sc.nextInt();
+        
+        if (choicePizzaNumber > 11 || choicePizzaNumber < 0) {
+            System.out.println("Enter Valid Pizza Number");
+            orderPizza();
+        } else if (choicePizzaNumber == 0) {
+            return;
+        } else {
+            
+            int quan;
+            do{
+                System.out.println("Enter Quantity");
+                quan = sc.nextInt();
+
+                if (quan < 0) {
+                    System.out.println("Enter Valid Quantity");
+                }
+            } while (quan < 0);
+
+            orderPizzaNumber[orderNumber] = choicePizzaNumber ;
+            orderPizzaquantity[orderNumber] = quan;
+            orderNumber++;
+        }
+    }
+    
+    public static void showCart() {
+        System.out.println("Your Card is : ");
+        for (int i = 0; i < orderPizzaNumber.length; i++) {
+            if (orderPizzaNumber[i] == 0) {
+                break;
+            }
+            System.out.println((i+1)+" "+item[orderPizzaNumber[i]-1]+" Quantity : "+orderPizzaquantity[i]);
+        }
+    }
+    
+    public static void billGenerate() {
+        int totalBill = 0;
+
+        for (int i = 0; i < orderPizzaNumber.length; i++) {
+            if (orderPizzaNumber[i] == 0) {
+                break;
+            }
+            totalBill += price[orderPizzaNumber[i]-1] * orderPizzaquantity[i];
+        }
+
+        end(totalBill);
+    }
+    
+
+    public static void end(int totalBill) {
+        System.out.println("----------------------------------------------------------------");
 		System.out.println();
 		
 		System.out.println("Name:"+name);
 		System.out.println("Mobile Number:"+mobile_number);
 		System.out.println();
 		System.out.println("*-*-*-*-* Your Order is *-*-*-*-*");
-		for(int i=0;i<order.length;i++)
-		{
-			if(order[i]!=0)
-			{
-				int q=order[i];
-				System.out.println("  "+item[q-1]+" * "+order_qun[i]+" = "+tempbill[i]+"inr");
-			}
-		}
-		System.out.println("     Your Total Bill Is:"+bill+"inr");
+		showCart();
+		System.out.println("     Your Total Bill Is:"+totalBill+"₹");
 		System.out.println();
 		
-		System.out.println("  ~ THANKS FOR VISIT OUR SHOP ~");
-	}
-
+        System.out.println("  ~ THANKS FOR VISIT OUR SHOP ~");
+        System.exit(0);
+    }
+    
 }
-
-class Main
-{
-	public static void main(String args[])
-	{
-		Project th=new Project();
-		th.welcome();
-	}
-}
- 
